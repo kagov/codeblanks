62f6596e8d1f7b4ab115867255223b8f01d366e2
kagov <kaushik.rajan4@gmail.com>
2021-02-14 23:46:26 +0000

    Auth theme added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { CreateAccountComponent } from './core/create-account/create-account.component';
+import { ForgotPasswordComponent } from './core/forgot-password/forgot-password.component';
+import { LoginComponent } from './core/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', component: LoginComponent },
+  { path: 'create-account', component: CreateAccountComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'app', loadChildren: () => import('./private/private.module').then(m => m.PrivateModule) },
+  { path: '**', redirectTo: 'login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
